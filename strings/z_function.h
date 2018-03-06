89e0ccdf65e3814a5f435c96156b6ef2c307d90e
Nikita Uvarov <dragonicthebest@gmail.com>
2018-03-06 22:29:04 +0300

    Normal z-functions (half-intervals, returns vector)

--- strings/z_function.h
+++ strings/z_function.h
@@ -1,17 +1,22 @@
-void zFunction(const string& s, vector<int>& z) {
-	int n = s.length();
-	if (n == 0)
-		return;
-	z[0] = 0;
-	int l = 0, r = 0;
-	for (int i = 1; i < n; ++i) {
-		if (i <= r)
-			z[i] = min(z[i - l], r - i + 1);
-		while (i + z[i] < n && s[i + z[i]] == s[z[i]])
-			++z[i];
-		if (i + z[i] - 1 > r) {
-			l = i;
-			r = i + z[i] - 1;
-		}
-	}
+vector<int> zFunction(string s) {
+    int n = s.length();
+
+    vector<int> z(n);
+    int l = 0, r = 0;
+    for (int i = 1; i < n; ++i) {
+        z[i] = max(min(z[i - l], r - i), 0);
+
+        while (i + z[i] < n && s[i + z[i]] == s[z[i]])
+            ++z[i];
+
+        if (i + z[i] > r) {
+            l = i;
+            r = i + z[i];
+        }
+    }
+    
+    if (n)
+        z[0] = n;
+
+    return z;
 }
