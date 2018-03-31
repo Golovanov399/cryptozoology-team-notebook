8fd48e19431991e78f725f227a641745ec5276d9
Nikita Uvarov <dragonicthebest@gmail.com>
2018-03-31 23:33:05 +0300

    add aho-corasick

---
+++ strings/aho_corasick.cpp
@@ -0,0 +1,50 @@
+const int ALPHABET = 26;
+
+struct state {
+    array<int, ALPHABET> transition = {};
+    int link = 0;
+    
+    bool isTerminal;
+};
+
+struct automaton {
+    vector<state> states = { state() };
+    int numStates = 1;
+    
+    void addString(const string& s) {
+        int cur = 0;
+        for (char c: s) {
+            c -= 'a';
+            int& to = states[cur].transition[c];
+            if (to) {
+                cur = to;
+            }
+            else {
+                cur = to = states.size();
+                states.push_back(state());
+            }
+        }
+        states[cur].isTerminal = true;
+    }
+    
+    void build() {
+        deque<int> q;
+        q.push_back(0);
+        
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop_front();
+            states[v].isTerminal = states[v].isTerminal || states[states[v].link].isTerminal;
+            
+            for (int c = 0; c < ALPHABET; ++c) {
+                if (int u = states[v].transition[c]) {
+                    states[u].link = v ? states[states[v].link].transition[c] : 0;
+                    q.push_back(u);
+                }
+                else {
+                    states[v].transition[c] = states[states[v].link].transition[c];
+                }
+            }
+        }
+    }
+};
